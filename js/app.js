135ed74ef3b6845c5298727fce4da0a78bde7ce1
amuammer <amuammer@students.iugaza.edu.ps>
2020-05-13 17:35:06 +0300

    change path of the script

--- js/app.js
+++ js/app.js
@@ -4,14 +4,16 @@ document.getElementById("cartTitle").innerText = `${userName} cart!`;
 
 
 function changeTotal(input) {
-   const row  = input.parentElement.parentElement.childNodes;
-   const price = row[7].innerText;
+   const row  = input.parentElement.parentElement;
+   const rowNodes = row.childNodes;
+   const price = rowNodes[7].innerText;
    const priceInt = parseInt(price, 10);
    const inputValue = parseInt(input.value, 10);
-   row[11].innerText = `$${inputValue * price}`; // row 11 is total price 
+   rowNodes[11].innerText = `$${inputValue * price}`; // row 11 is total price 
     
-  // const totalElement = document.getElementById("total");
-  // const total = totalElement.innerText.split(" ")[1];
-  //totalElement.innerText = `Total ${(parseInt(total, 10) + priceInt )}`
+
+   // const totalElement = document.getElementById("total");
+   //const total = totalElement.innerText.split(" ")[1];
+  // totalElement.innerText = `Total ${(parseInt(total, 10) + priceInt )}`
    
 }
