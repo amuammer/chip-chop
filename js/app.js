aed4fab9445e8bd4d8462bd79f4679bbc324455e
amuammer <amuammer@students.iugaza.edu.ps>
2020-05-14 02:48:41 +0300

    adding delete item

--- js/app.js
+++ js/app.js
@@ -10,10 +10,9 @@ function changeTotal(input) {
    const priceInt = parseInt(price, 10);
    const inputValue = parseInt(input.value, 10);
    rowNodes[11].innerText = `$${inputValue * price}`; // row 11 is total price 
-    
+}
 
-   // const totalElement = document.getElementById("total");
-   //const total = totalElement.innerText.split(" ")[1];
-  // totalElement.innerText = `Total ${(parseInt(total, 10) + priceInt )}`
-   
+function deleteItem(btn) {
+   const row  = btn.parentElement.parentElement;
+   row.parentNode.removeChild(row);  
 }
